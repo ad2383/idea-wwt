7c6949e04a2e03804fcd06d95b081fc93f824376
Alona Dhal <alonadhal@gmail.com>
2026-03-26 11:36:33 -0400

    Add files via upload

---
+++ wwt.m
@@ -0,0 +1,17 @@
+load all_data.csv
+load energyDataset.mat
+load waterTreatmentData.mat
+
+% linear regression
+
+% generalized linear regression
+
+% gaussian process regression
+
+% support vector machine
+
+% decision tree
+
+% ensemble of learners
+
+% generalize adeditive model
