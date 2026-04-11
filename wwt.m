86664ddfa887024514c1d7348c175c02413671dc
Soham Bhagat <sb2787@scarletmail.rutgers.edu>
2026-04-10 22:09:15 -0400

    feat: updated script to display all input and output pairs

--- wwt.m
+++ wwt.m
@@ -10,14 +10,13 @@ algorithmNames = {'Linear Regression', 'Generalized Linear', 'Gaussian Process',
     'Support Vector Machine', 'Decision Tree', 'Ensemble', 'Generalized Additive'};
 n = size(inputs, 1);
 
-disp('Training...');
-
-% 80/20 split
 idx = randperm(n);
 trainIdx = idx(1:round(0.8*n));
-testIdx = idx(round(0.8*n)+1:end);
+testIdx  = idx(round(0.8*n)+1:end);
+originalTrainIdx = trainIdx;
+originalTestIdx  = testIdx;
 trainInputs = inputs(trainIdx, :);
-testInputs = inputs(testIdx, :);
+testInputs  = inputs(testIdx,  :);
 
 rmseResults = zeros(7, 5);
 timeResults = zeros(7, 5);
@@ -26,10 +25,11 @@ for i = 1:7
     for j = 1:5
         output = outputs{j};
         trainOutput = output(trainIdx);
-        testOutput = output(testIdx);
-        [rmseResults(i,j), timeResults(i,j), ~] = trainAlgorithm(trainInputs, trainOutput, testInputs, testOutput, algorithmNames{i});
+        testOutput  = output(testIdx);
+        [rmseResults(i,j), timeResults(i,j), ~] = trainAlgorithm( ...
+            trainInputs, trainOutput, testInputs, testOutput, algorithmNames{i});
     end
-    disp(['Trained: ' algorithmNames{i}]);
+    disp(['  Trained: ' algorithmNames{i}]);
 end
 
 disp('RMSE results:');
@@ -37,16 +37,10 @@ rmseTable = [table(algorithmNames', 'VariableNames', {'Algorithm'}), ...
              array2table(rmseResults, 'VariableNames', outputNames)];
 disp(rmseTable);
 
-disp('Elapsed time results:');
+disp('Training time results:');
 timeTable = [table(algorithmNames', 'VariableNames', {'Algorithm'}), ...
              table(sum(timeResults, 2), 'VariableNames', {'TotalSeconds'})];
 disp(timeTable);
-
-% Save original 80/20 split indices for normalization section
-originalTrainIdx = trainIdx;
-originalTestIdx = testIdx;
-
-disp('Split ratios...');
 
 splitRatios = [0.5, 0.6, 0.7, 0.8, 0.9];
 rmseByRatio = zeros(5, 7);
@@ -55,81 +49,162 @@ for r = 1:5
     ratio = splitRatios(r);
     idx = randperm(n);
     splitTrainIdx = idx(1:round(ratio*n));
-    splitTestIdx = idx(round(ratio*n)+1:end);
-    
-    trainInputs = inputs(splitTrainIdx, :);
-    testInputs = inputs(splitTestIdx, :);
+    splitTestIdx  = idx(round(ratio*n)+1:end);
+
+    splitTrainInputs = inputs(splitTrainIdx, :);
+    splitTestInputs  = inputs(splitTestIdx,  :);
     trainOutput = DO_1(splitTrainIdx);
-    testOutput = DO_1(splitTestIdx);
-    
+    testOutput  = DO_1(splitTestIdx);
+
     for i = 1:7
-        rmseByRatio(r,i) = trainAlgorithm(trainInputs, trainOutput, testInputs, testOutput, algorithmNames{i});
+        rmseByRatio(r,i) = trainAlgorithm( ...
+            splitTrainInputs, trainOutput, splitTestInputs, testOutput, algorithmNames{i});
     end
-    disp(['Tested: ' num2str(ratio*100) '% split']);
+    disp(['  Tested: ' num2str(ratio*100) '/' num2str((1-ratio)*100) ' split']);
 end
 
-splitLabels = cell(5, 1);
+splitLabels = cell(5,1);
 for r = 1:5
-    splitLabels{r} = [num2str(splitRatios(r)*100) '%'];
+    splitLabels{r} = [num2str(splitRatios(r)*100) '/' num2str((1-splitRatios(r))*100)];
 end
 splitTable = [table(splitLabels, 'VariableNames', {'Split'}), ...
               array2table(rmseByRatio, 'VariableNames', algorithmNames)];
 disp(splitTable);
 
-disp('Plotting...');
-
-imageDir = fullfile(scriptDir, 'images');
-
-for i = 1:7
-    plotWithErrors(Q_inf, DO_1, algorithmNames{i}, imageDir);
-end
-disp('  Saved 7 plots to images/');
-
-disp('Normalization...');
-
-% Restore original 80/20 split
 trainIdx = originalTrainIdx;
-testIdx = originalTestIdx;
+testIdx  = originalTestIdx;
 
-% Normalize inputs using training data
 trainMean = mean(inputs(trainIdx, :));
-trainStd = std(inputs(trainIdx, :));
+trainStd  = std(inputs(trainIdx,  :));
 trainInputsNorm = (inputs(trainIdx, :) - trainMean) ./ trainStd;
-testInputsNorm = (inputs(testIdx, :) - trainMean) ./ trainStd;
+testInputsNorm  = (inputs(testIdx,  :) - trainMean) ./ trainStd;
 
 rmseNorm = zeros(7, 5);
 
 for i = 1:7
     for j = 1:5
         output = outputs{j};
-        
-        outputMean = mean(output(trainIdx));
-        outputStd = std(output(trainIdx));
+
+        outputMean      = mean(output(trainIdx));
+        outputStd       = std(output(trainIdx));
         trainOutputNorm = (output(trainIdx) - outputMean) ./ outputStd;
-        testOutputNorm = (output(testIdx) - outputMean) ./ outputStd;
-        testOutputOrig = output(testIdx);
-        
-        [~, ~, trainedModel] = trainAlgorithm(trainInputsNorm, trainOutputNorm, testInputsNorm, testOutputNorm, algorithmNames{i});
-        
-        % Transform predictions to original scale
-        predOutput = predict(trainedModel, testInputsNorm) * outputStd + outputMean;
-        
+        testOutputNorm  = (output(testIdx)  - outputMean) ./ outputStd;
+        testOutputOrig  = output(testIdx);
+
+        [~, ~, trainedModel] = trainAlgorithm( ...
+            trainInputsNorm, trainOutputNorm, testInputsNorm, testOutputNorm, algorithmNames{i});
+
+        predOutput    = predict(trainedModel, testInputsNorm) * outputStd + outputMean;
         rmseNorm(i,j) = sqrt(mean((testOutputOrig - predOutput).^2));
     end
-    disp(['Trained (normalized): ' algorithmNames{i}]);
+    disp(['  Trained (normalized): ' algorithmNames{i}]);
 end
 
-originalRmse = mean(rmseResults, 2);
-normalizedRmse = mean(rmseNorm, 2);
-percentChange = (normalizedRmse - originalRmse) ./ originalRmse * 100;
-normTable = table(algorithmNames', originalRmse, normalizedRmse, percentChange, ...
-    'VariableNames', {'Algorithm', 'Original', 'Normalized', 'ChangePct'});
+origAvg   = mean(rmseResults, 2);
+normAvg   = mean(rmseNorm, 2);
+pctChange = (normAvg - origAvg) ./ origAvg * 100;
+normTable = table(algorithmNames', origAvg, normAvg, pctChange, ...
+    'VariableNames', {'Algorithm','Original','Normalized','ChangePct'});
 disp(normTable);
 
+pairs = {
+    Q_air_3, 'Q_{air,3}', 'Qair3', '(m^3/day)', DO_1, 'DO_1';
+    Q_air_4, 'Q_{air,4}', 'Qair4', '(m^3/day)', DO_2, 'DO_2';
+    Q_air_5, 'Q_{air,5}', 'Qair5', '(m^3/day)', DO_3, 'DO_3';
+    Q_air_5, 'Q_{air,5}', 'Qair5', '(m^3/day)', NO3,  'NO3';
+    Q_air_5, 'Q_{air,5}', 'Qair5', '(m^3/day)', NH4,  'NH4';
+    Temp,    'Temp',      'Temp',  '(C)',        DO_3, 'DO_3';
+    Temp,    'Temp',      'Temp',  '(C)',        NO3,  'NO3';
+    Temp,    'Temp',      'Temp',  '(C)',        NH4,  'NH4';
+};
+
+nPairs   = size(pairs, 1);
+imageDir = fullfile(scriptDir, 'images');
+
+for i = 1:7
+    folderName = strrep(algorithmNames{i}, ' ', '_');
+    if ~isfolder(fullfile(imageDir, folderName))
+        mkdir(fullfile(imageDir, folderName));
+    end
+end
+
+for p = 1:nPairs
+    inputCol    = pairs{p,1};
+    inputLabel  = pairs{p,2};
+    inputShort  = pairs{p,3};
+    inputUnit   = pairs{p,4};
+    outputCol   = pairs{p,5};
+    outputLabel = pairs{p,6};
+
+    pairTitle = [inputLabel ' -> ' outputLabel];
+    disp(['--- Pair: ' pairTitle ' ---']);
+
+    pairRMSE = zeros(7,1);
+    pairTime = zeros(7,1);
+
+    for i = 1:7
+        trainInput  = inputCol(originalTrainIdx);
+        testInput   = inputCol(originalTestIdx);
+        trainOutput = outputCol(originalTrainIdx);
+        testOutput  = outputCol(originalTestIdx);
+
+        [pairRMSE(i), pairTime(i), ~] = trainAlgorithm( ...
+            trainInput, trainOutput, testInput, testOutput, algorithmNames{i});
+
+        plotWithErrors(inputCol, outputCol, algorithmNames{i}, imageDir, ...
+            inputLabel, inputUnit, outputLabel, inputShort, originalTrainIdx, originalTestIdx);
+    end
+
+    pairRMSETable = table(algorithmNames', pairRMSE, pairTime, ...
+        'VariableNames', {'Algorithm','RMSE','TrainSec'});
+    disp(pairRMSETable);
+
+    pairSplitRMSE = zeros(5, 7);
+    for r = 1:5
+        ratio = splitRatios(r);
+        idx = randperm(n);
+        splitTrainIdx = idx(1:round(ratio*n));
+        splitTestIdx  = idx(round(ratio*n)+1:end);
+
+        for i = 1:7
+            pairSplitRMSE(r,i) = trainAlgorithm( ...
+                inputCol(splitTrainIdx), outputCol(splitTrainIdx), ...
+                inputCol(splitTestIdx),  outputCol(splitTestIdx), algorithmNames{i});
+        end
+    end
+
+    pairSplitTable = [table(splitLabels, 'VariableNames', {'Split'}), ...
+                      array2table(pairSplitRMSE, 'VariableNames', algorithmNames)];
+    disp(pairSplitTable);
+
+    inputMean = mean(inputCol(originalTrainIdx));
+    inputStd = std(inputCol(originalTrainIdx));
+    trainInputNorm = (inputCol(originalTrainIdx) - inputMean) ./ inputStd;
+    testInputNorm = (inputCol(originalTestIdx)  - inputMean) ./ inputStd;
+
+    outputMean = mean(outputCol(originalTrainIdx));
+    outputStd = std(outputCol(originalTrainIdx));
+    trainOutputNorm = (outputCol(originalTrainIdx) - outputMean) ./ outputStd;
+    testOutputNorm = (outputCol(originalTestIdx)  - outputMean) ./ outputStd;
+    testOutputOrig = outputCol(originalTestIdx);
+
+    pairNormRMSE = zeros(7,1);
+    for i = 1:7
+        [~, ~, mdl] = trainAlgorithm( ...
+            trainInputNorm, trainOutputNorm, testInputNorm, testOutputNorm, algorithmNames{i});
+        pred = predict(mdl, testInputNorm) * outputStd + outputMean;
+        pairNormRMSE(i) = sqrt(mean((testOutputOrig - pred).^2));
+    end
+
+    pctChg = (pairNormRMSE - pairRMSE) ./ pairRMSE * 100;
+    pairNormTable = table(algorithmNames', pairRMSE, pairNormRMSE, pctChg, ...
+        'VariableNames', {'Algorithm','OriginalRMSE','NormalizedRMSE','ChangePct'});
+    disp(pairNormTable);
+end
 
 function [rmse, elapsed, model] = trainAlgorithm(trainInputs, trainOutput, testInputs, testOutput, algoName)
     tic;
-    
+
     if strcmp(algoName, 'Linear Regression')
         model = fitlm(trainInputs, trainOutput);
     elseif strcmp(algoName, 'Generalized Linear')
@@ -145,35 +220,42 @@ function [rmse, elapsed, model] = trainAlgorithm(trainInputs, trainOutput, testI
     elseif strcmp(algoName, 'Generalized Additive')
         model = fitrgam(trainInputs, trainOutput);
     end
-    
+
     elapsed = toc;
     predOutput = predict(model, testInputs);
     rmse = sqrt(mean((testOutput - predOutput).^2));
 end
 
-function plotWithErrors(inputCol, outputCol, algoName, saveDir)
-    [inputSorted, sortIdx] = sort(inputCol);
-    outputSorted = outputCol(sortIdx);
-    
-    [~, ~, trainedModel] = trainAlgorithm(inputCol, outputCol, inputCol, outputCol, algoName);
-    
-    predOutput = predict(trainedModel, inputSorted);
-    mse = mean((outputSorted - predOutput).^2);
-    
+function plotWithErrors(inputCol, outputCol, algoName, imageDir, inputLabel, inputUnit, outputLabel, inputShort, trainIdx, testIdx)
+    trainInput = inputCol(trainIdx);
+    testInput = inputCol(testIdx);
+    trainOutput = outputCol(trainIdx);
+    testOutput = outputCol(testIdx);
+
+    [~, ~, trainedModel] = trainAlgorithm( ...
+        trainInput, trainOutput, testInput, testOutput, algoName);
+
+    [testSorted, sortIdx] = sort(testInput);
+    outputSorted = testOutput(sortIdx);
+    predSorted = predict(trainedModel, testSorted);
+    rmse = sqrt(mean((outputSorted - predSorted).^2));
+
     fig = figure('Visible', 'off');
-    plot(inputSorted, outputSorted, 'o', 'MarkerSize', 3);
+    plot(testSorted, outputSorted, 'o', 'MarkerSize', 3);
     hold on;
-    plot(inputSorted, predOutput, 'm', 'LineWidth', 2);
-    
-    for i = 1:50:length(inputSorted)
-        plot([inputSorted(i) inputSorted(i)], [predOutput(i), outputSorted(i)], 'r');
+    plot(testSorted, predSorted, 'm', 'LineWidth', 2);
+
+    for i = 1:50:length(testSorted)
+        plot([testSorted(i) testSorted(i)], [predSorted(i), outputSorted(i)], 'r');
     end
-    
-    xlabel('Q_{inf} (m^3/day)');
-    ylabel('DO_1 (mg/L)');
-    title([algoName ' - MSE: ' num2str(round(mse, 4))]);
+
+    xlabel([inputLabel ' ' inputUnit]);
+    ylabel([outputLabel ' (mg/L)']);
+    title([algoName ' — RMSE: ' num2str(round(rmse, 4))]);
     legend('Data', 'Model', 'Error', 'Location', 'best');
-    
-    saveas(fig, fullfile(saveDir, ['singleInput_' algoName '.png']));
+
+    folderName = strrep(algoName, ' ', '_');
+    fname = ['singleInput_' inputShort '_' outputLabel '.png'];
+    saveas(fig, fullfile(imageDir, folderName, fname));
     close(fig);
 end
